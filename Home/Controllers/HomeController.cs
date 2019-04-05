bc64fdd16a97fe69c6189382903f6374ea32b9d4
Kabirf70 <kabirf70@gmail.com>
2019-04-06 00:03:50 +0600

    first commit

---
+++ Home/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Home.Controllers
+{
+    public class HomeController : ApiController
+    {
+        private static List<string> city = new List<string> { "dhaka", "Mymensingh", "bogra" };
+
+        public IEnumerable<string> Get()
+        {
+            return city;
+        }
+        public string Get(int id)
+        {
+            return city[id];
+        }
+    }
+}
